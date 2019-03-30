df0fd0bba2ef08a12222515baaa9581da135ba58
Wesley Oates <wesley.oates@gmail.com>
2019-03-30 09:11:06 -0600

    Consolidated unit tests from another unit test project.

---
+++ UnitTests/TestArgumentParsing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arguments;
+
+namespace HelpersTest
+{
+    [TestClass]
+    public class TestArgumentParsing
+    {
+        [TestMethod]
+        public void Test_Options_Parsing()
+        {
+            TestOptions opts = Arguments.Options.Parse<TestOptions>("BoolArg", "StringArgA=valueA", "StringArgB", "valueB");
+            Assert.AreEqual(opts.BoolArg, true);
+            Assert.AreEqual(opts.StringArgA, "valueA");
+            Assert.AreEqual(opts.StringArgB, "valueB");
+            Console.WriteLine("Whatevah.");
+        }
+
+        public class TestOptions
+        {
+            [Pattern(ArgumentPattern.KeyOnly)]
+            [Group("GroupA", true)]
+            public bool BoolArg;
+
+            [Pattern(ArgumentPattern.ValueRequired)]
+            [Group("GroupA", true)]
+            public string StringArgA;
+
+            [Pattern(ArgumentPattern.ValueRequired)]
+            [Group("GroupA", true)]
+            public string StringArgB;
+             
+            
+        }
+    }
+}
