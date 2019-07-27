7131ddcc970f6ab2282ff0858a18f2d8ac9b594b
Wesley Oates <wesley.oates@gmail.com>
2019-07-27 15:20:44 -0600

    WeakReferenceSet - Validated addition, removal, and iteration

--- DataStructures/Sets/WeakReferenceSet.cs
+++ DataStructures/Sets/WeakReferenceSet.cs
@@ -10,26 +10,25 @@ namespace DataStructures
 {
     public sealed class WeakReferenceSet<T> : ICollection<T> where T : class
     {
-        private readonly List<Node> _Head = new List<Node>();
+        private readonly List<Node> _Head = new List<Node> { null };
         private readonly Random _Random = new Random(0);
-        private int _MaxLinks;
 
-        public WeakReferenceSet(int capacity = 0, int seed = 0)
-        {
-            this._MaxLinks = (capacity < 1) ? 1 : Mathematics.Int32.Log_2(capacity);
+        public WeakReferenceSet( int seed = 0)
+        {   
             this._Random = new Random(seed);
         }
 
         public bool Add(T item)
         {
+            int itemHash = item.GetHashCode();
+            if (GetNode(item, itemHash, out Node[] prevNodes)) return false;
+
             int linkSize = 1;
-            for (int i = 1; i < _MaxLinks && (_Random.Next() & 1) != 1; i++) linkSize++;
-            Node newNode = new Node(item, item.GetHashCode(), linkSize);
-            while (newNode.Next.Length > _Head.Count) _Head.Add(null);
+            for (int i = 1; i < _Head.Count && (_Random.Next() & 1) != 1; i++) linkSize++;
+            Node newNode = new Node(item, itemHash, linkSize);
 
-            if (GetNode(item, out Node[] prevNodes, newNode.HashCode)) return false;
-
-            for (int lvl = 0; lvl < newNode.Next.Length; lvl++)
+            int maxLevels = Math.Min(prevNodes.Length, newNode.Next.Length);
+            for (int lvl = 0; lvl < maxLevels; lvl++)
             {
                 Node prevAtLevel = prevNodes[lvl];
                 newNode.Prev[lvl] = prevAtLevel;
@@ -47,95 +46,79 @@ namespace DataStructures
 
                 if (oldNext != null) { oldNext.Prev[lvl] = newNode; newNode.Next[lvl] = oldNext; }
             }
-            if (++Count > (1 << _MaxLinks)) _MaxLinks++;
+            while (newNode.Next.Length > _Head.Count)
+                _Head.Add(newNode);
+            if (++Count >= (1 << _Head.Count))
+                _Head.Add(null);
             return true;
         }
 
         public void Clear()
         {
             _Head.Clear();
+            _Head.Add(null);
             Count = 0;
-            _MaxLinks = 1;
         }
 
-        public bool Contains(T item) => GetNode(item, out _);
+        public bool Contains(T item) => GetNode(item, item.GetHashCode(), out _);
 
         public int Count { get; private set; }
 
         public bool Remove(T item)
         {
-            if (!GetNode(item, out Node[] prev)) return false;
-            Compact(prev[0]);
+            if (!GetNode(item, item.GetHashCode(), out Node[] prev)) return false;
+            Remove(prev[0]);
             return true;
         }
-
-        private bool GetNode(T item, out Node[] prev, int itemHash = int.MinValue)
+        
+        private bool GetNode(T item, int itemHash, out Node[] trail)
         {
-            prev = _Head.ToArray();
-            if (prev.Length == 0) return false;
+            int level = _Head.Count - 1;
+            trail = new Node[_Head.Count];
+            if (trail.Length == 0) return false;
 
-            if (itemHash == int.MinValue) itemHash = item.GetHashCode();
+            // Find the topmost node referenced in Head.
+            while (_Head[level] == null)
+                if (--level < 0) return false;
 
-            int level = prev.Length - 1;
-
-            while (prev[level] != null && prev[level].HashCode > itemHash) { prev[level] = null; if (--level < 0) return false; }
-            Node node = prev[level];
+            // Continue descending while Head's references are higher than the item.
+            Node node;
+            T nodeData;
             while (true)
             {
-                while (node == null || node.HashCode > itemHash)
-                {
-                    if (level <= 0) return false;
-                    node = prev[--level];
-                }
-                if (node.HashCode == itemHash)
-                {
-                    if (!node.Data.TryGetTarget(out T existing))
-                    {
-                        Node toRemove = node;
-                        if ((node = node.Prev[level]) == null)
-                        {
-                            while (_Head[level] == null)
-                            {
-                                prev[level] = null;
-                                if (--level < 0) return false;
-                            }
-                            node = _Head[level];
-                        }
-                        Compact(toRemove);
-                    }
-                    else if (existing.Equals(item)) return true;
-                    else
-                    {
-                        for (; level > 0; level--) prev[level] = node;
-                        node = node.Next[0];
-                    }
-                }
-                else
-                {
-                    while (node.Next[level] == null || node.Next[level].HashCode > itemHash)
-                    {
-                        prev[level] = node;
-                        if (--level < 0) return false;
-                    }
-                    node = node.Next[level];
-                    prev[level] = node;
-                }
+                node = _Head[level];
+                if (!node.Data.TryGetTarget(out nodeData)) Remove(node);
+                else if (node.HashCode <= itemHash) break;
+                else if (--level < 0) return false;
             }
+
+            // Okay, there's now a node to hang our hat on.
+            while (level >= 0)
+            {
+                if (item.Equals(nodeData)) { for (; level >= 0; level--) trail[level] = node; return true; }
+
+                Node next = node.Next[level];
+                if (next == null || next.HashCode > itemHash) trail[level--] = node;
+                else if (!next.Data.TryGetTarget(out T nextData)) Remove(next);                
+                else { node = next; nodeData = nextData; }
+            }
+            return nodeData.Equals(item);
         }
 
-        private void Compact(Node node)
+        private void Remove(Node node)
         {
+            
             for (int i = 0; i < node.Next.Length; i++)
             {
                 Node next = node.Next[i];
                 Node prev = node.Prev[i];
-                if (next != null) next.Prev[i] = node.Prev[i];
-                if (prev != null) prev.Next[i] = node.Next[i];
+                if (next != null) next.Prev[i] = prev;
+                if (prev != null) prev.Next[i] = next;
+                else _Head[i] = next;
             }
-            if (--Count < (1 << (_MaxLinks - 1)))
-                if (--_MaxLinks < 1)
-                    _MaxLinks = 1;
+            Count--;
         }
+       
 
         void ICollection<T>.Add(T item) => this.Add(item);
 
@@ -161,7 +144,7 @@ namespace DataStructures
                 {
                     Node toRemove = n;
                     n = n.Next[0];
-                    Compact(toRemove);
+                    Remove(toRemove);
                 }
                 else
                 {
--- UnitTests/Test_WeakReferenceSet.cs
+++ UnitTests/Test_WeakReferenceSet.cs
@@ -2,45 +2,48 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DataStructures;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace UnitTests
 {
     [TestClass]
     public class Test_WeakReferenceSet
     {
-        [TestMethod]
-        public void TestAdd()
+        private List<Integer> _Integers = new List<Integer>();
+        [TestInitialize]
+        public void Initialize()
         {
-            List<Integer> list = new List<Integer>();
-            for (int i = 0; i < 20; i++)
-            {
-                list.Add(new Integer(i));
-            }
-
+            for (int i = 0; i < 100; i++) _Integers.Add(new Integer(i));
+        }
+        
+        [TestMethod]
+        public void Test_Add()
+        {
+            
             WeakReferenceSet<Integer> wrsA = new WeakReferenceSet<Integer>(0);
             for (int i = 0; i < 20; i++)
             {
-                Assert.IsTrue(wrsA.Add(list[i]));
+                Assert.IsTrue(wrsA.Add(_Integers[i]));
             }
             Assert.AreEqual(20, wrsA.Count);
             for (int i = 0; i < 20; i++)
             {
-                Assert.IsFalse(wrsA.Add(list[i]));
+                Assert.IsFalse(wrsA.Add(_Integers[i]));
             }
             Assert.AreEqual(20, wrsA.Count);
             int idx = 0;
             foreach (Integer val in wrsA)
             {
-                Assert.AreEqual(val, list[idx++]);
+                Assert.AreEqual(val, _Integers[idx++]);
             }
 
-            Common.Permute(list);
+            Common.Permute(_Integers);
             WeakReferenceSet<Integer> wrsB = new WeakReferenceSet<Integer>(0);
-            foreach (Integer i in list)
+            foreach (Integer i in _Integers)
                 Assert.IsTrue(wrsB.Add(i));
 
             // Since an Integer's hash is its value, this should put them in order.
-            Integer[] sorted = list.ToArray();
+            Integer[] sorted = _Integers.ToArray();
             Array.Sort(sorted, (a, b) => a.Value.CompareTo(b.Value));
             idx = 0;
             foreach (Integer val in wrsB)
@@ -49,13 +52,96 @@ namespace UnitTests
             }
         }
 
+        [TestMethod]
+        public void Test_Compaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Test_Iteration()
+        {
+            WeakReferenceSet<Integer> wrsA = new WeakReferenceSet<Integer>(0);
+            var random = new List<Integer>(_Integers);
+            Common.Permute(random);
+            for (int i = 0; i < random.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Add(random[i]));
+            }
+            for (int i = 0; i < random.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Contains(random[i]));
+            }
+
+            int idx = 0;
+            foreach (Integer i in wrsA)
+            {
+                Assert.AreEqual(_Integers[idx++], i);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Remove()
+        {
+            WeakReferenceSet<Integer> wrsA = new WeakReferenceSet<Integer>(0);
+            for (int i = 0; i < _Integers.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Add(_Integers[i]));
+            }
+            for (int i = 0; i < _Integers.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Contains(_Integers[i]));
+            }
+            Assert.AreEqual(_Integers.Count, wrsA.Count);
+
+            for  (int  i =0; i < _Integers.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Remove(_Integers[i]));
+            }
+            for (int i = 0; i < _Integers.Count; i++)
+            {
+                Assert.IsFalse(wrsA.Contains(_Integers[i]));
+            }
+            Assert.AreEqual(0, wrsA.Count);
+
+
+            // Now do it randomly.
+            Common.Permute(_Integers);
+            for (int i = 0; i < _Integers.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Add(_Integers[i]));
+            }
+            for (int i = 0; i < _Integers.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Contains(_Integers[i]));
+            }
+            Common.Permute(_Integers);
+            for (int i = 0; i < _Integers.Count; i++)
+            {
+                Assert.IsTrue(wrsA.Remove(_Integers[i]));
+            }
+            for (int i = 0; i < _Integers.Count; i++)
+            {
+                Assert.IsFalse(wrsA.Contains(_Integers[i]));
+            }
+            Assert.AreEqual(0, wrsA.Count);
+
+        }
+
+
+
+
+
         private class Integer
         {
             public readonly int Value;
+            [DebuggerStepThrough]
             public Integer(int value) { this.Value = value; }
             public override string ToString() => Value.ToString();
+            [DebuggerStepThrough]
             public override int GetHashCode() => Value;
-            public override bool Equals(object obj) => Value == ((Integer)obj).Value;
+            [DebuggerStepThrough]
+            public override bool Equals(object obj) => base.Equals(obj);
         }
     }
 }
