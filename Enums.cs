939e6bea623a7840c21fd950c201f4e28e60456f
Wesley Oates <wesley.oates@gmail.com>
2020-03-20 19:57:30 -0600

    Added LoggingSeverity

---
+++ Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public enum LoggingSeverity
+    {
+        CRITICAL,
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR
+    }
+}
