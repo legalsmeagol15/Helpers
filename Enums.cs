b85cd7ac2db18e4af5765bab2874c7ee2d575801
Wesley Oates <wesley.oates@gmail.com>
2020-03-22 20:58:07 -0600

    Reorganized logging in Helpers

--- Enums.cs
+++ Enums.cs
@@ -6,12 +6,5 @@ using System.Threading.Tasks;
 
 namespace Helpers
 {
-    public enum LoggingSeverity
-    {
-        DEBUG = 10,
-        INFO = 20,
-        WARNING = 30,
-        ERROR = 40,
-        CRITICAL = 50
-    }
+    
 }
