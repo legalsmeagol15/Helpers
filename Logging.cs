040892d47f0de362e0de551371308d81792f0e7c
Wesley Oates <wesley.oates@gmail.com>
2020-04-26 05:03:27 -0700

    Created a static means of logging.

--- Logging.cs
+++ Logging.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +16,47 @@ namespace Helpers
         CRITICAL = 50
     }
 
-    public delegate void LogEventHander(object sender, LogEventArgs e);
+    public class Logging
+    {
+        private static Logging _Logger;
+        public static Logging Log
+        {
+            get => _Logger ?? (_Logger = new Logging());
+        }        
+
+        private Outputs _Outputs;
+        public enum Outputs { NONE = 0, EVENT = 1 << 1, FILE = 1 << 2 }
+        public void SetOutputs(Outputs outputs) { this._Outputs = outputs; }
+        public bool OutputsToEvent => (_Outputs & Outputs.EVENT) != Outputs.NONE;
+        public bool OutputsToFile => (_Outputs & Outputs.FILE) != Outputs.NONE;
+
+        public Logging(Outputs outputs = Outputs.EVENT | Outputs.FILE) { this._Outputs = outputs; }
+
+        public void Debug(string message, string facility = "fac1", [CallerMemberName] string prefix = null)
+            => Do_Log(facility, LoggingSeverity.DEBUG, message, prefix);
+        public void Info(string message, string facility = "fac1", [CallerMemberName] string prefix = null)
+            => Do_Log(facility, LoggingSeverity.INFO, message, prefix);
+        public void Warning(string message, string facility = "fac1", [CallerMemberName] string prefix = null)
+            => Do_Log(facility, LoggingSeverity.WARNING, message, prefix);
+        public void Error(string message, string facility = "fac1", [CallerMemberName] string prefix = null)
+            => Do_Log(facility, LoggingSeverity.ERROR, message, prefix);
+        public void Critical(string message, string facility = "fac1", [CallerMemberName] string prefix = null)
+            => Do_Log(facility, LoggingSeverity.CRITICAL, message, prefix);
+
+        private void Do_Log(string facility, LoggingSeverity severity, string message, string prefix, string separator = " - ")
+        {
+            if (prefix != null)
+                message = prefix + separator + message;
+            if (OutputsToEvent)
+                OnLog?.Invoke(this, new LogEventArgs(facility, severity, message));
+            if (OutputsToFile)
+                throw new NotImplementedException();
+        }
+
+        public event LogEventHandler OnLog;
+    }
+
+    public delegate void LogEventHandler(object sender, LogEventArgs e);
 
     public class LogEventArgs : EventArgs
     {
