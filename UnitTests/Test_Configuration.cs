1a5769ea13b776fb44f65b18cf04e7e17517362c
Wesley Oates <wesley.oates@gmail.com>
2021-02-06 14:23:00 -0700

    Small bug in unit test.

--- UnitTests/Test_Configuration.cs
+++ UnitTests/Test_Configuration.cs
@@ -11,7 +11,7 @@ namespace UnitTests
         public void InitializeConfigurationAttribute()
         {
             // The attrib Versions property uses the VersionIntervalSet, which is a new thing.
-            ConfigurationAttribute attrib = new ConfigurationAttribute(default, ">=1.2.3.4");
+            ConfigurationAttribute attrib = new ConfigurationAttribute(">=1.2.3.4");
             Assert.IsTrue(attrib.Versions.Contains(new Version(1, 2, 3, 4)));
             Assert.IsTrue(attrib.Versions.Contains(new Version(2, 0, 0, 0)));
             Assert.IsFalse(attrib.Versions.Contains(new Version(1, 2, 3, 3)));
